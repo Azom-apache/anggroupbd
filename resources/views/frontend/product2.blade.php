ff5442470cbb909256f941a2126b660bb851bdba
fxjakaria04 <freelancerx2023@gmail.com>
2026-03-10 22:31:32 +0600

    back button added

--- resources/views/frontend/product2.blade.php
+++ resources/views/frontend/product2.blade.php
@@ -1,9 +1,18 @@
 <script src="https://cdn.tailwindcss.com"></script>
 <x-frontend-layout title="Products">
 
+    
     <section class="sk__animated-header sk__header-y-m dark-shade-7-bg dark-shade-5-border sk__parallax-background-section sk__parallax-fixer-ignore-height" style="opacity: 1; transform: translate(0px, 0px);">
         <div class="text-center mb-5">
             <h2 class="display-4 text-dark">Our Products</h2>
+        </div>
+        <div class="container my-3">
+            <a href="javascript:history.back()" class="btn btn-outline-secondary btn-sm mb-4 d-inline-flex align-items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                </svg>
+                Back
+            </a>
         </div>
         <div class="container">
             <div class="row g-4">
